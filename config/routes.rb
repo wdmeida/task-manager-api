9b083a9593a175b41e8e766019c5b1fd096677bd
Wagner Almeida <wdmeida@gmail.com>
2017-05-10 23:56:54 +0000

    Disableing devise routes (temporarity)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'api_version_constraint'
 
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Config namespace and subdomain.
