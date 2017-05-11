60370c9ae52cf784d86bc0735bec8ecb3fd29891
Wagner Almeida <wdmeida@gmail.com>
2017-05-11 01:45:22 +0000

    Symbolizing response body keys

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,8 @@ class Api::V1::UsersController < ApplicationController
     
     def show
         begin
-            @user = User.find(params[:id])
-            respond_with @user
+            user = User.find(params[:id])
+            respond_with user
         rescue
             head 404
         end
