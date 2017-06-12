922813faa94183428cce0defbf373d6a2d689ee0
Wagner Almeida <wdmeida@gmail.com>
2017-06-12 21:31:12 +0000

    Adding create action to the tasks controller with tests

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -14,4 +14,19 @@ class Api::V1::TasksController < ApplicationController
       head :not_found
     end
   end
+
+  def create
+    task = current_user.tasks.build(task_params)
+    
+    if task.save
+      render json: task, status: :created
+    else
+      render json: { errors: task.errors }, status: :unprocessable_entity 
+    end
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title, :description, :deadline, :done)
+    end
 end
